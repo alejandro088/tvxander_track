71f5f9a54f5fda3afe7a53fb1d945a4546360d2e
alejandro <snackbarchat@gmail.com>
2019-01-22 13:05:10 -0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Tmdb\Laravel\TmdbServiceProvider::class,
+        App\Providers\TmdbServiceProvider::class,
 
     ],
 
