bbcac1000eeac900ae0920bc258fbc628d7ff5ee
Alejandro Cayssials <admin@webxander.com>
2021-02-08 15:13:20 -0300

    add tmdb client into constructor BaseController

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,10 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use App\Services\Tmdb\Client;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $client;
+
+    function __construct()
+    {
+        $client = (new Client())->handle(); 
+
+
+        $this->client = $client;
+    }
 }
