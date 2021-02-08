8321ec8772ae00c174ce041f9fc85980fba32fd4
Alejandro Cayssials <admin@webxander.com>
2021-02-08 15:20:18 -0300

    add routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShowController;
+use App\Http\Controllers\MovieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/movies/discover/{get}/{movie?}', [MovieController::class, 'list']);
+
+Route::get('/tv/discover/{get}/{tv?}', [ShowController::class, 'list']);
+
+Route::get('/movies/latest', [MovieController::class, 'latest']);
+
+Route::get('/movies/toprated', [MovieController::class, 'toprated']);
+
+Route::get('/movies/upcoming', [MovieController::class, 'upcoming']);
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\ShowController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\EpisodeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,8 +27,44 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+})->name('home');
+
+Route::get('/movies/show/{movie}', [MovieController::class, 'show'])->name('movie.show');
+
+Route::get('/search', [SearchController::class, 'search'])->name('search');
+
+Route::get('/search/grid', [SearchController::class, 'grid'])->name('search.grid');
+
+Route::get('/tv/show/{tv}', [ShowController::class, 'show'])->name('tv.show');
+
+Route::middleware(['auth:sanctum', 'verified'])->post('/tv/{show}/add', [ShowController::class, 'store'])->name('tv.store');
+
+Route::middleware(['auth:sanctum', 'verified'])->post('/tv/{show}/archive',  [ShowController::class, 'archive'])->name('tv.archive');
+
+Route::middleware(['auth:sanctum', 'verified'])->delete('/tv/{show}',  [ShowController::class, 'destroy'])->name('tv.delete');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard/favoritemovies', [UserController::class, 'myFavoriteMovies'])->name('user.favorite.movies');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard/calendar', function () {
+    return Inertia::render('Profile/Calendar');
+})->name('user.calendar');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard/myshows',[UserController::class, 'myshows'])->name('user.myshows');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard/unwatched',[UserController::class, 'unwatched'])->name('user.unwatched');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard/shows/{id}', [ShowController::class, 'listShow'])->name('tv.listShow');
+
+Route::middleware(['auth:sanctum', 'verified'])->post('/dashboard/season/{season}/watched', [EpisodeController::class, 'setWatchedSeason'])->name('tv.season.watched');
+
+Route::middleware(['auth:sanctum', 'verified'])->post('/dashboard/episode/{episode}/watched', [EpisodeController::class, 'setWatched'])->name('tv.episode.watched');
+
+Route::middleware(['auth:sanctum', 'verified'])->post('/dashboard/favorite/{source}', [UserController::class, 'addToFavorite'])->name('tv.favorite');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/events', [ShowController::class, 'events'])->name('tv.events');
+
+
