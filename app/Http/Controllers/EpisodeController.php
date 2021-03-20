ac1c9e35a132e678b6a1f3e8e21f58a9be7fe28a
Alejandro Cayssials <admin@webxander.com>
2021-03-20 05:16:16 -0300

    fix some js files

--- app/Http/Controllers/EpisodeController.php
+++ app/Http/Controllers/EpisodeController.php
@@ -9,6 +9,7 @@ class EpisodeController extends Controller
 {
     public function setWatchedSeason(Request $request, $seasonid)
     {
+        
         return Episode::where('season_id', $seasonid)
                 ->update(['watched' => $request->check]);
     }
